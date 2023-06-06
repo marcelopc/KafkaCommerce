4c8ba1a3c2a90a52323fa183c1298edf4c0b0d7a
marcelo <marcelopc04@gmail.com>
2023-06-06 16:59:26 -0300

    chore

--- Dockerfile
+++
@@ -1,3 +0,0 @@
-FROM node:latest as base
-
-RUN npm update && npm i -g pnpm
--- custom.d.ts
+++
@@ -1,9 +0,0 @@
-import { type CreateUserType } from '@core/types/user/userTypes'
-
-declare global{
-  namespace Express {
-    export interface Request {
-      user?: CreateUserType
-    }
-  }
-}
